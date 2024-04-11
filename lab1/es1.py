e50b28d1ae5af0f947a7b3473ec10c9e80aec061
Enrico <enricofen300@gmail.com>
2024-04-11 09:52:39 +0200

    es1

--- lab1/es1.py
+++ lab1/es1.py
@@ -16,5 +16,39 @@ def euclidean_algorithm(a, b):
     m=m-1
     return (q,r[m])
 
-print("gcd(57,93):")
-print(euclidean_algorithm(57,93))
+a=int(input("Insert the first number: "))
+b=int(input("Insert the second number: "))
+print(f"gcd({a},{b}):")
+print(euclidean_algorithm(a,b))
+
+"""
+now we have to find s and t such that 57s + 93t = gcd(57, 93)
+"""
+
+def extended_euclidean_algorithm(a, b):
+    A = [a]
+    B = [b]
+    T = [0]
+    t = 1
+    S = [1]
+    s = 0
+    q = A[0] // B[0]
+    r = A[0] - q * B[0]
+    while r > 0:
+        temp = T[0] - q * t
+        T[0] = t
+        t = temp
+        temp = S[0] - q * s
+        S[0] = s
+        s = temp
+        A[0] = B[0]
+        B[0] = r
+        q = A[0] // B[0]
+        r = A[0] - q * B[0]
+    r = B[0]
+    return (r, s, t)
+
+
+print("extended_euclidean_algorithm:")
+(r,s,t) = extended_euclidean_algorithm(a,b)
+print(f"57*{s} + 93*{t} = {r}")
--- lab1/es2.py
+++ lab1/es2.py
@@ -2,32 +2,33 @@
 Multiplicative inverse and the EXTENDED
 EUCLIDEAN ALGORITHM
 Compute the following multiplicative inverses:
-(a) 17^−1 mod 101
-(b) 357^−1 mod 1234
-(c) 3125^−1 mod 9987.
+(a) 17^-1 mod 101
+(b) 357^-1 mod 1234
+(c) 3125^-1 mod 9987.
 """
-
-def extended_euclidean_algorithm(a, b):
+def mult_inverse(a,b):
     A = [a]
     B = [b]
     T = [0]
-    t=1
-    S = [0]
-    q=A[0]//B[0]
-    r = A[0] - q*B[0]
-    while r>0:
-        temp = T[0]-q*t
-        s=temp
-        T[0]=t
-        t=temp
-        temp = S[0]-q*s
-        S[0]=s
-        A[0]=B[0]
-        q=A[0]//B[0]
-        r=A[0]-q*B[0]
-    return (r,s,t)
+    t = 1
+    q = A[0]//B[0]
+    r = A[0]-(q*B[0])
+
+    while r > 0:
+        tmp = (T[0]-(q*t)) % a
+        T[0] = t
+        t = tmp
+        A[0] = B[0]
+        B[0] = r
+        q = A[0] // B[0]
+        r = A[0] - (q * B[0])
+    if B[0] != 1:
+        print("b has no inverse modulo a" )
+        return -1
+    else:
+        return t
 
 print("extended_euclidean_algorithm:")
-print(extended_euclidean_algorithm(17^-1,101))
-print(extended_euclidean_algorithm(357^-1,1234))
-print(extended_euclidean_algorithm(3125^-1,9987))
+print(mult_inverse(17,101))
+print(mult_inverse(357,1234))
+print(mult_inverse(3125,9987))
